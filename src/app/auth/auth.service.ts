408f73be7fae5427ea3ad321c8f92440e13c937b
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2023-04-07 08:38:51 +0200

    Save the user's auth status in the localStorage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,10 +9,20 @@ export class AuthService {
   private readonly admin_password = 'password';
   isLoggedIn = false;
 
-  constructor() {}
+  constructor() {
+    const storedValue = localStorage.getItem('loggedIn');
+    if (storedValue) {
+      this.isLoggedIn = JSON.parse(storedValue);
+    }
+  }
 
   login(data: {email: string, password: string}) {
     const isValid = data.email === this.admin_email && data.password === this.admin_password;
-    return of(isValid).pipe(delay(1000), tap((isValid) => this.isLoggedIn = isValid));
+    return of(isValid).pipe(delay(1000), tap((isValid) => {
+      this.isLoggedIn = isValid
+      if (this.isLoggedIn) {
+        localStorage.setItem('loggedIn', 'true');
+      }
+    }));
   }
 }
